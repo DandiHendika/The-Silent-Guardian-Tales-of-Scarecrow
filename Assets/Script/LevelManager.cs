d775232df7098c7b4d907af09bfdf79877f55747
DandiHendika <113397399+DandiHendika@users.noreply.github.com>
2024-12-04 10:03:43 +0700

    test

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -7,6 +7,12 @@ using UnityEngine.UI;
 public class LevelManager : MonoBehaviour
 {
     public Button[] levelButtons; // Referensi untuk tombol level di UI
+    public Sprite spriteDefault; // Sprite untuk tombol interactable
+    public Sprite spriteDisabled; // Sprite untuk tombol non-interactable
+    public Sprite spriteDefault2; // Sprite untuk tombol interactable
+    public Sprite spriteDisabled2; // Sprite untuk tombol non-interactable
+    public Sprite spriteDefault3; // Sprite untuk tombol interactable
+    public Sprite spriteDisabled3; // Sprite untuk tombol non-interactable
 
     void Start()
     {
@@ -19,10 +25,16 @@ public class LevelManager : MonoBehaviour
             if (i + 1 <= unlockedLevel)
             {
                 levelButtons[i].interactable = true; // Aktifkan tombol
+                levelButtons[0].GetComponent<Image>().sprite = spriteDefault;
+                levelButtons[1].GetComponent<Image>().sprite = spriteDefault2;
+                levelButtons[2].GetComponent<Image>().sprite = spriteDefault3;
             }
             else
             {
                 levelButtons[i].interactable = false; // Nonaktifkan tombol
+                levelButtons[0].GetComponent<Image>().sprite = spriteDisabled;
+                levelButtons[1].GetComponent<Image>().sprite = spriteDisabled2;
+                levelButtons[2].GetComponent<Image>().sprite = spriteDisabled3;
             }
         }
     }
